ccb4190d932420bf9a712c038edffb99da26c7a3
Andres0919 <andres.posada0919@gmail.com>
2018-11-26 22:50:58 -0500

    learning about terminal params

---
+++ 03-bases-node/app.js
@@ -0,0 +1,10 @@
+const { crearArchivo } = require('./multiplicar/multiplicar.js');
+
+let base = 'asda';
+
+// crearArchivo(base)
+//     .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+//     .catch(err => console.log(err));
+
+console.log(process.argv);
+
