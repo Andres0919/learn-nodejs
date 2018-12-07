d9711d9ee48a8f8afeaa3c996374068e33e19af1
Andres0919 <andres.posada0919@gmail.com>
2018-12-06 23:49:37 -0500

    axios with google and weather API

---
+++ 05-clima-mundo/lugar/lugar.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const getLugarLatLng = async (direccion) => {
+
+    let encodeUrl = encodeURI(direccion);
+
+    let res = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=AIzaSyDyJPPlnIMOLp20Ef1LlTong8rYdTnaTXM`)
+
+    if (res.data.status === 'ZERO_RESULTS') {
+        throw new Error(`No hay resultados para la ciudad ${ direccion } `)
+    }
+
+    let location = res.data.results[0];
+    let coors = location.geometry.location;
+
+    return {
+        direccion: location.formatted_address,
+        lat: coors.lat,
+        lng: coors.lng
+    }
+}
+
+
+module.exports = {
+    getLugarLatLng
+}
